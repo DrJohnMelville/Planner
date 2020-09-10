3bb7918fe71e696bac24ebea2f27f88919863c8c
John Melville <JohnMelville@gmail.com>
2020-09-10 05:30:24 -0400

    Basic structure for the task Model

--- Planner.Models.Test/Tasks/PlannerTaskTest.cs
+++ Planner.Models.Test/Tasks/PlannerTaskTest.cs
@@ -9,9 +9,46 @@ namespace Planner.Models.Test.Tasks
         [Fact]
         public void TaskHasName()
         {
+            Assert.Equal("", sut.Name);
             sut.Name = "Foo";
             Assert.Equal("Foo", sut.Name);
         }
+
+        [Fact]
+        public void TaskHasPriority()
+        {
+            Assert.Equal(' ', sut.Priority);
+            sut.Priority = 'A';
+            Assert.Equal('A', sut.Priority);
+        }
+
+        [Fact]
+        public void TaskHasOrder()
+        {
+            Assert.Equal(0, sut.Order);
+            sut.Order = 12;
+            Assert.Equal(12, sut.Order);
+            
+        }
+
+        [Fact]
+        public void TaskHasStatus()
+        {
+            Assert.Equal(PlannerTaskStatus.Incomplete, sut.Status);
+            sut.Status = PlannerTaskStatus.Cancelled;
+            Assert.Equal(PlannerTaskStatus.Cancelled, sut.Status);
+               
+        }
+
+        [Fact]
+        public void TaskHasStatusDetail()
+        {
+            Assert.Equal("", sut.StatusDetail);
+            sut.StatusDetail = "7/28/1975";
+            Assert.Equal("7/28/1975", sut.StatusDetail);
+            
+        }
+
 
     }
 }
--- Planner.Models/Tasks/PlannerTask.cs
+++ Planner.Models/Tasks/PlannerTask.cs
@@ -1,7 +1,22 @@
-﻿namespace Planner.Models.Tasks
+﻿using System.Collections.Generic;
+
+namespace Planner.Models.Tasks
 {
+    public enum PlannerTaskStatus
+    {
+        Incomplete = 0,
+        Done = 1,
+        Deferred = 2,
+        Delegated =3,
+        Pending = 4,
+        Cancelled = 5
+    }
     public class PlannerTask
     {
-        public string Name { get; set; }
+        public string Name { get; set; } = "";
+        public char Priority { get; set; } = ' ';
+        public int Order { get; set; }
+        public PlannerTaskStatus Status { get; set; }
+        public string StatusDetail { get; set; } = "";
     }
 }
