34c5642804e9c3732ef3bec5986654cda91f4961
John Melville <JohnMelville@gmail.com>
2020-09-14 04:28:32 -0400

    Defines a Note

---
+++ Planner.Models/NamespaceBugfix.cs
@@ -0,0 +1,8 @@
+﻿namespace Melville.Generated
+{
+    public class NamespaceBugfix
+    {
+        //Until Rider recognizes generated code, this will prevent the "nonexistant" namespace from being
+        // deleted when rider extracts to a new class.
+    }
+}
---
+++ Planner.Models/Tasks/Note.cs
@@ -0,0 +1,13 @@
+﻿using NodaTime;
+using Melville.Generated;
+
+namespace Planner.Models.Tasks
+{
+    public partial class Note
+    {
+        [AutoNotify] private LocalDate date;
+        [AutoNotify] private int index;
+        [AutoNotify] private string name = "";
+        [AutoNotify] private string rtfText = "";
+    }
+}
