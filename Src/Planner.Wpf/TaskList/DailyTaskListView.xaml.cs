17d439fb794f46623faf7e264dfaf0b94e721ed1
John Melville <JohnMelville@gmail.com>
2020-09-14 05:31:54 -0400

    Downgrade to nercore 3.1 due to limited wpf Support in .net 5.0

---
+++ Src/Planner.Wpf/TaskList/DailyTaskListView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Planner.Wpf.TaskList
+{
+    public partial class DailyTaskListView : UserControl
+    {
+        public DailyTaskListView()
+        {
+            InitializeComponent();
+        }
+    }
+}
