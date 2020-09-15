cb54c6ea2ff3e49c1b4784473646d894212dc248
John Melville <JohnMelville@gmail.com>
2020-09-14 20:00:41 -0400

    Work on the task list UI.  Needs to fix attributes so that they work with multiple assemblies.

--- Src/Planner.Wpf/TaskList/DailyTaskListViewModel.cs
+++ Src/Planner.Wpf/TaskList/DailyTaskListViewModel.cs
@@ -1,12 +1,40 @@
-﻿namespace Planner.Wpf.TaskList
+﻿using System.Collections.Generic;
+using System.Windows.Data;
+using Melville.MVVM.AdvancedLists.PersistentLinq;
+using Planner.Models.Tasks;
+using Melville.Generated;
+
+namespace Planner.Wpf.TaskList
 {
     public class DailyTaskListViewModel
     {
-        
+        private readonly PlannerTaskList sourceList;
+        public CollectionView TaskItems { get; }
+        private bool isRankingTasks;
+
+        private static void AddFakeData(PlannerTaskList src)
+        {
+            src.Add(new PlannerTask() {Name = "Task1"});
+            src.Add(new PlannerTask() {Name = "Task2"});
+            src.Add(new PlannerTask() {Name = "Task3"});
+            src.Add(new PlannerTask() {Name = "Task4"});
+        }
+
+        public DailyTaskListViewModel()
+        {
+            var src = new PlannerTaskList();
+            AddFakeData(src);
+            TaskItems = new ListCollectionView(src.SelectCol(i => new PlannerTaskViewModel(i)));
+        }
     }
 
-    public class TaskListSorterViewModel
+    public class PlannerTaskViewModel
     {
-        
+        public PlannerTask PlannerTask {get;}
+
+        public PlannerTaskViewModel(PlannerTask plannerTask)
+        {
+            PlannerTask = plannerTask;
+        }
     }
 }
