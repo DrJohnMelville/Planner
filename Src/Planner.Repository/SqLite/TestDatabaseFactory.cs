025dd69f27d250da472267db8ca58d0d51fb72dd
John Melville <jmelvill@hotmail.com>
2020-10-02 05:55:45 -0400

    The fake database always has a task for Today.

--- Src/Planner.Repository/SqLite/TestDatabaseFactory.cs
+++ Src/Planner.Repository/SqLite/TestDatabaseFactory.cs
@@ -2,6 +2,7 @@
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using NodaTime;
+using NodaTime.Extensions;
 using Planner.Models.Repositories;
 
 namespace Planner.Repository.SqLite
@@ -28,8 +29,8 @@ namespace Planner.Repository.SqLite
 
             context.PlannerTasks.Add(new RemotePlannerTask(Guid.NewGuid())
             {
-                Date = new LocalDate(2020, 10, 1),
-                Name = "My Birthday"
+                Date = SystemClock.Instance.InTzdbSystemDefaultZone().GetCurrentDate(),
+                Name = "Sample Task"
             });
             context.SaveChanges();
         }
