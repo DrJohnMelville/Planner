6a4996b1d2db92b3f3cbb847fcedc4901242b328
John Melville <jmelvill@hotmail.com>
2020-10-09 17:01:14 -0400

    Beginning work on a rich text block.

---
+++ Src/Planner.Wpf/TaskList/RichTextBlock.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Melville.WpfControls.Buttons;
+
+namespace Planner.Wpf.TaskList
+{
+    public class RichTextBlock: Control
+    {
+        static RichTextBlock()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(RichTextBlock), 
+                new FrameworkPropertyMetadata(typeof(RichTextBlock)));
+        }
+    }
+}
