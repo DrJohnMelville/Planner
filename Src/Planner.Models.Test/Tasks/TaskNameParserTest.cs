4f9191b6b92a3029cea62b886252243a85f9cb40
John Melville <JohnMelville@gmail.com>
2020-10-09 20:10:05 -0400

    Beginning to work on a text segmenter

---
+++ Src/Planner.Models.Test/Tasks/TaskNameParserTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using Planner.Models.Tasks;
+using Xunit;
+
+namespace Planner.Models.Test.Tasks
+{
+    public class TaskNameParserTest
+    {
+        private readonly TaskNameParser sut = new TaskNameParser();
+
+        [Fact]
+        public void TestName()
+        {
+            var list = sut.Parse("Foo");
+            Assert.Single(list);
+            
+        }
+
+    }
+}
---
+++ Src/Planner.Models/Tasks/TaskNameParser.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Text.RegularExpressions;
+
+namespace Planner.Models.Tasks
+{
+    public class SegmentDecl<T>
+    {
+        public Regex Recognizer { get; }
+        public T Label {get;}
+
+        public SegmentDecl(Regex recognizer, T label)
+        {
+            Recognizer = recognizer;
+            Label = label;
+        }
+    }
+
+    public class Segment<T>
+    {
+        public string Text { get; }
+        public T Label { get; }
+
+        public Segment(string text, T label)
+        {
+            Text = text;
+            Label = label;
+        }
+    }
+
+    public class StringSegmenter<T>
+    {
+        private SegmentDecl<T>[] declarations;
+
+        public StringSegmenter(SegmentDecl<T>[] declarations)
+        {
+            this.declarations = declarations;
+        }
+
+        public IEnumerable<Segment<T>> Parse(string text, T defaultLabel)
+        {
+            yield return new Segment<T>(text, defaultLabel);
+        }
+    }
+
+    public enum TaskTextType
+    {
+        NoLink
+    }
+    public class TaskNameParser
+    {
+        private StringSegmenter<TaskTextType> declarations = new StringSegmenter<TaskTextType>(
+            new SegmentDecl<TaskTextType>[]
+            {
+            }
+            );
+        
+        public IEnumerable<Segment<TaskTextType>> Parse(string text)
+        {
+            return declarations.Parse(text, TaskTextType.NoLink);
+        }
+    }
+}
