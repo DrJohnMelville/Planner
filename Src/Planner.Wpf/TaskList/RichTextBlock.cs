ce5015f955a33c6b419162ed4a2a2b861f333e54
John Melville <JohnMelville@gmail.com>
2020-10-12 05:30:57 -0400

    Some refactoring on the RichTextBlock

--- Src/Planner.Wpf/TaskList/RichTextBlock.cs
+++ Src/Planner.Wpf/TaskList/RichTextBlock.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Globalization;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Input;
 using System.Windows.Markup;
 using System.Windows.Media;
 using Melville.MVVM.Wpf.EventBindings.SearchTree;
@@ -21,68 +23,65 @@ namespace Planner.Wpf.TaskList
         
         public static readonly DependencyProperty TextProperty = DependencyProperty.Register(
             "Text", typeof(string), typeof(RichTextBlock),
-            new FrameworkPropertyMetadata(null));
+            new FrameworkPropertyMetadata(null, OnTextChanged));
 
+        private TextBlock? target;
+        private static void OnTextChanged(DependencyObject d, 
+            DependencyPropertyChangedEventArgs e)
+        {
+            ((RichTextBlock) d).OnTextChanged(e.NewValue?.ToString()??"");
+        }
+
+        public void InitializeDisplay(TextBlock target)
+        {
+            this.target = target;
+            OnTextChanged(Text);
+        }
+
+        private void OnTextChanged(string toString)
+        {
+            RichRenderer.Instance.FillTextBlock(toString, target);
+        }
+        
         public string Text
         {
             get => (string) GetValue(TextProperty);
             set => SetValue(TextProperty, value);
         }
 
-        public static readonly DependencyProperty RenderedTBProperty =
-            DependencyProperty.Register("RenderedTB", typeof(TextBlock), typeof(RichTextBlock),
-                new FrameworkPropertyMetadata(null));
 
-        public TextBlock RenderedTB
+        public void MouseDoubleClickEvent(Grid grid)
         {
-            get => (TextBlock) GetValue(RenderedTBProperty);
-            set => SetValue(RenderedTBProperty, value);
+            var tb = (TextBox) grid.Children.OfType<TextBox>().First();
+            tb.Focus();
+            Keyboard.Focus(tb);
         }
 
-        public RichTextBlock()
-        {
-            BindingOperations.SetBinding(this, RenderedTBProperty, new Binding()
-            {
-                Path = new PropertyPath("Text"),
-                Source = this,
-                Converter = RichRenderer.Instance
-            });
-        }
     }
-    public class RichRenderer: IValueConverter
+    public class RichRenderer
     {
         public static readonly RichRenderer Instance = new RichRenderer();
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => 
-            RenderString(value?.ToString() ?? "");
-
         private static readonly TaskNameParser parser = new TaskNameParser();
-
-        private TextBlock RenderString(string source)
+        public void FillTextBlock(string source, TextBlock? ret)
         {
-            var ret = new TextBlock() {Background = Brushes.White};
+            if (ret == null) return;
+            ret.Inlines.Clear();
             var ac = (IAddChild) ret;
             foreach (var span in parser.Parse(source))
             {
                 var run = new Run(span.Text);
-                // if (span.Label != TaskTextType.NoLink)
-                // {
-                //     run.Foreground = Brushes.DodgerBlue;
-                //     run.TextDecorations.Add(TextDecorations.Underline)
-                // }
-                ac.AddChild(span.Label == TaskTextType.NoLink?(Inline)run:CreateLink(run, span.Label));
+                ac.AddChild(span.Label == TaskTextType.NoLink ? (Inline) run : CreateLink(run, span.Label));
             }
-            return ret;
         }
 
         private Hyperlink CreateLink(Run run, TaskTextType label)
         {
             var ret = new Hyperlink(run);
+            ret.Background= Brushes.Transparent;
+            ret.IsEnabled = true;
             ret.Click += (s, e) => RunOnVisualTreeSearch.Run((DependencyObject)s, 
                 label.ToString(), new object[] {e}, out var _);
             return ret;
         }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) => 
-            DependencyProperty.UnsetValue;
     }
 }
