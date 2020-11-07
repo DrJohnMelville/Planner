c4af691be4cfb2584d1905bde93106e8feea594a
John Melville <JohnMelville@gmail.com>
2020-11-07 06:49:16 -0500

    Loading notifications work in the journla view

---
+++ Src/Planner.Wpf/PlannerPages/NoteBrowser.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using CefSharp.Wpf;
+
+namespace Planner.Wpf.PlannerPages
+{
+    public class NoteBrowser:ChromiumWebBrowser
+    {
+        public static readonly DependencyProperty IsNavigatingProperty = DependencyProperty.Register("IsNavigating",
+            typeof(bool), typeof(NoteBrowser), new FrameworkPropertyMetadata(false));
+
+        public bool IsNagivating
+        {
+            get => (bool) GetValue(IsNavigatingProperty);
+            set => SetValue(IsNavigatingProperty, value);
+        }
+        public NoteBrowser()
+        {
+            this.LoadingStateChanged += (s, e) =>
+            {
+                Dispatcher.Invoke(() => IsNagivating = e.IsLoading);
+            };
+        }
+    }
+}
