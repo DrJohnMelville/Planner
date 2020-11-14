721f514e7d047d046818df43bf2ef5da987fa08e
John Melville <JohnMelville@gmail.com>
2020-11-13 20:15:36 -0500

    Month popup displays but needs to do something when links are clicked.

--- Src/Planner.Blazor/ModalComponent/ModalService.cs
+++ Src/Planner.Blazor/ModalComponent/ModalService.cs
@@ -9,8 +9,9 @@ namespace Planner.Blazor.ModalComponent
         public event Action? OnClose;
         
 
-        public void Show<T>(string title) where T : ComponentBase => Show(title, typeof(T));
-        public void Show(string title, Type contentType)
+        public void Show<T>(string title, params (string Name, object Value)[] parameters) 
+            where T : ComponentBase => Show(title, typeof(T), parameters);
+        public void Show(string title, Type contentType, params (string Name, object Value)[] parameters)
         {
             if (contentType.BaseType != typeof(ComponentBase))
             {
@@ -20,6 +21,11 @@ namespace Planner.Blazor.ModalComponent
             var content = new RenderFragment(x =>
             {
                 x.OpenComponent(1, contentType);
+                int i = 2;
+                foreach (var parameter in parameters)
+                {
+                    x.AddAttribute(i++, parameter.Name, parameter.Value);
+                }
                 x.CloseComponent();
             });
 
