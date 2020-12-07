fb74b912d54c3f4105c76ce649e87a41c5e236b0
John Melville <jmelvill@hotmail.com>
2020-12-07 15:50:21 -0500

    Appointment data declarations.

---
+++ Src/Planner.Models/Appointments/Appointment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Melville.INPC;
+using NodaTime;
+
+namespace Planner.Models.Appointments
+{
+    public partial class Appointment
+    {
+        [AutoNotify] private Instant start;
+        [AutoNotify] private Instant end;
+        [AutoNotify] private Guid details;
+    }
+
+    public partial class AppointmentDetails
+    {
+        [AutoNotify] private string title = "";
+        [AutoNotify] private string location = "";
+        [AutoNotify] private string bodyText = "";
+        [AutoNotify] private string uniqueOutlookId = "";
+        
+    }
+}
